425d5b9f61890d9928fd6387c325414025ecae18
Atharva Sen <atharvasenbarai@gmail.com>
2022-05-18 23:23:39 +0530

    Update Following.js

--- src/pages/Following.js
+++ src/pages/Following.js
@@ -43,21 +43,8 @@ const Following = () => (
                             Go Back to home
                         </Link>
                     </p>
-        <div className="">
-            <div>
-                <div className="flex md:items-center space-x-2 sm:space-x-12 pr-4 ">
-                    <div className="w-4/12 lg:w-3/12 flex flex-col justify-center items-center sm:flex-none">
-                        <img
-                            className="h-30 w-30 sm:h-36  sm:w-36 md:h-48 md:w-48 rounded-full
-						  ml-0 sm:ml-12 md:ml-12 lg:ml-16"
-                            src={photo || './images/avatars/placeholder.png'}
-                            alt="profile"
-                        />
-                        <div className="mt-4 md:hidden text-center ml-2 sm:ml-0">
-                            <h1 className="font-bold">{displayName}</h1>
-                            <h1>{email}</h1>
-                        </div>
-                    </div>
+        
+                       
                     <div className="w-8/12 lg:w-9/12">
                         <div className="md:flex md:items-center space-y-4 md:space-y-0 md:space-x-4">
                             <h1 className="text-3xl md:4xl font-normal">{username}</h1>
@@ -90,32 +77,17 @@ const Following = () => (
                             ) : null}
                         </div>
                         <div className="hidden w-6/12 md:flex justify-between my-3">
-                            <h1>
-                                <span className="font-bold">{totalPost}</span> Post
-                            </h1>
-                            <h1>
-                                <span className="font-bold">{totalFollowers}</span> Followers
-                            </h1>
+                            
                             <h1>
                                 <span className="font-bold">{following.length}</span> Following
                             </h1>
                         </div>
-                        <div className="hidden md:block">
-                            <h1 className="font-bold">{displayName}</h1>
-                            <h1>{email}</h1>
-                        </div>
+                        
                     </div>
                 </div>
                 <div className="flex justify-between px-12 mt-12 border border-gray-border py-2 text-center text-sm text-gray-base md:hidden">
                     <div className="text-center">
-                        <p className="font-bold">{totalPost}</p>
-                        <p>Post</p>
-                    </div>
-                    <div>
-                        <p className="font-bold">{totalFollowers}</p>
-                        <p>Followers</p>
-                    </div>
-                    <div>
+                     
                         <p className="font-bold">{following.length}</p>
                         <p>Following</p>
                     </div>
