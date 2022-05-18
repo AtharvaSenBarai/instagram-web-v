b56723709c282caa06707a1a9b865e29d9e81e0f
Atharva Sen <atharvasenbarai@gmail.com>
2022-05-18 21:47:08 +0530

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -12,9 +12,9 @@ const NotFound = () => (
             <Header />
             <div className="container mx-auto max-w-screen-lg bg-white mt-24 flex justify-center text-center">
                 <div className="w-w/6  sm:w-4/6 ">
-                    <h1 className="text-xl font-medium mb-2">Sorry, this page is not available.</h1>
+                    <h1 className="text-xl font-medium mb-2">Opps... </h1>
                     <p>
-                        The link you followed may be broken, or the page may have been removed.{' '}
+                        The page which you want to visit is not found....{' '}
                         <Link className="pointer text-blue" to="/">
                             Go Back to home
                         </Link>
