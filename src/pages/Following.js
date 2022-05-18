459f4d78b110ced6b1cfdcfc5b776e3b8e08bf53
Atharva Sen <atharvasenbarai@gmail.com>
2022-05-18 23:27:41 +0530

    Update Following.js

--- src/pages/Following.js
+++ src/pages/Following.js
@@ -28,12 +28,13 @@ const ProfileHeader = ({ user, totalPost }) => {
     return (
 
 const Following = () => (
+
+        <div className="h-screen overflow-auto">
+            <Header />
     <div>
         <Helmet>
             <title>Detagram | Following</title>
         </Helmet>
-        <div className="h-screen overflow-auto">
-            <Header />
             <div className="container mx-auto max-w-screen-lg bg-white mt-24 flex justify-center text-center">
                 <div className="w-w/6  sm:w-4/6 ">
                     <h1 className="text-xl font-medium mb-2">Accounts You Follow</h1>
