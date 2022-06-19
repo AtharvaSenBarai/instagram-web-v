f94d1971bd85bf14ad527489a758c7aaf6445776
Atharva Sen <atharvasenbarai@gmail.com>
2022-06-19 14:51:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import ProtectedRoute from './helpers/ProtectedRoute';
 import useAuthListener from './hooks/useAuthListener';
 import ChatBoard from './pages/ChatBoard';
 import Following from './pages/Following';
-import Home from './pages/home';
 import Dashboard from './pages/Dashboard';
 import EditProfile from './pages/EditProfile';
 import Explore from './pages/Explore';
@@ -26,7 +25,6 @@ function App() {
                         <ProtectedRoute loggedInUser={loggedInUser} path="/" exact>
                             <Route component={Dashboard} />
                         </ProtectedRoute>
-<Route path="/home" component={Home} />
                         <Route path="/login" component={Login} />
                         <Route path="/registration" component={Registration} />
                         <Route path="/notfound" component={NotFound} />
